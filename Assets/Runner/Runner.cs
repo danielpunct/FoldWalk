dd9c0c78c34505781230e080d9fd57b547e889d2
Daniel Lupascu <liviudaniel.lupascu@gmail.com>
2019-09-04 00:05:18 +0300

    lean touch; gamelogic; some mouse logic

---
+++ Assets/Runner/Runner.cs
@@ -0,0 +1,12 @@
+﻿using Gamelogic.Extensions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Runner : Singleton<Runner>
+{
+    public void StartMove(Vector2 direction)
+    {
+        Debug.Log(direction);
+    }
+ }
